343fe3be1cc7d81bf1ffd6fdb666b098db3cfc32
oyerohabib <oyerohabib@yahoo.com>
2025-02-19 22:54:33 +0100

    fix: changed .tsx to .jsx to fix build issue

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App";
 import "./globals.css";
 import { ToastContainer } from "react-toastify";
+import App from "./App";
 
-createRoot(document.getElementById("root")!).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
     <ToastContainer />
