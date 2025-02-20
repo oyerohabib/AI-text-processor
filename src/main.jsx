8959f7c026e19ecf6c157754955d841b498e6cf9
oyerohabib <oyerohabib@yahoo.com>
2025-02-20 12:10:39 +0100

    chore: used .env secrets instead of hardcoding them into project

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,18 @@ import "./globals.css";
 import { ToastContainer } from "react-toastify";
 import App from "./App";
 
+// Adding LANGUAGE_DETECTOR_API KEY to the head element
+const LANGUAGE_EL = document.createElement("meta");
+LANGUAGE_EL.httpEquiv = "origin-trial";
+LANGUAGE_EL.content = import.meta.env.VITE_LANGUAGE_DETECTOR_API;
+document.head.appendChild(LANGUAGE_EL);
+
+// Adding SUMMARIZE_API KEY to the head element
+const SUMMARIZE_EL = document.createElement("meta");
+SUMMARIZE_EL.httpEquiv = "origin-trial";
+SUMMARIZE_EL.content = import.meta.env.VITE_SUMMARIZE_API;
+document.head.appendChild(SUMMARIZE_EL);
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
